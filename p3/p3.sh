af396fb03ef2cb4c3a37778cec411ff4a115384a
Crabb Marsha <cmarsha@ge-m5.kzn.21-school.ru>
2023-05-02 22:38:35 +0300

    some estetik

--- p3/p3.sh
+++ p3/p3.sh
@@ -3,8 +3,7 @@
 
 sudo apt-get update &&
 sudo apt-get -y upgrade &&
-
-sudo apt-get install curl net-tools -y
+sudo apt-get install curl net-tools -y &&
 
 # НУЖНО локально ставить аргоцд??? https://yashguptaa.medium.com/application-deploy-to-kubernetes-with-argo-cd-and-k3d-8e29cf4f83ee
 
@@ -17,43 +16,55 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
+printf "==========\nInstalling docker\n==========\n"
+curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh &&
 
+sudo usermod -aG docker $USER
+
+printf "==========\nInstalling kubectl\n==========\n"
 # Download latest stable version of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
-
 # Download sha256 hash sum for just installed kubectl and check it
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256" &&
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check &&
-
 # Install kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
+
+# Install k3d
+printf "==========\nInstalling k3d\n==========\n"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash &&
 
 # Add autocompletion
 echo "source <(k3d completion bash)" >> ~/.bashrc
 source ~/.bashrc
 
-
-# Install docker
-curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh &&
-
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
-
-
 # Install cluster
 # https://www.sokube.io/en/blog/gitops-on-a-laptop-with-k3d-and-argocd-en
 
+printf "==========\nCreating cluster\n==========\n"
 # sudo k3d cluster create mycluster -p "8080:30080@agent:0" --agents 2
 sudo k3d cluster create iot &&
+printf "==========\nCreating namespaces\n==========\n"
 sudo kubectl create namespace argocd &&
+sudo kubectl create namespace dev &&
 
 # Install ArgoCD into argocd NAMESPACE
 # https://yashguptaa.medium.com/application-deploy-to-kubernetes-with-argo-cd-and-k3d-8e29cf4f83ee
 
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-# sudo kubectl wait --for=condition=Ready pods --all -n argocd
-sleep 30 #??? for pods gets up
-# Получить пароль в декодированном виде:
+sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml &&
+printf "==========\nWaiting for pods\n"
+sleep 40 && #??? for pods gets up
+printf "==========\n"
+# OR
+# sudo kubectl wait --for=condition=Ready pods --all -n argocd &&
+
+# check nodes on argocd namespace
+printf "==========\nGet pods\n==========\n"
+sudo docker ps &&
+sudo kubectl get pods -n argocd &&
+sudo kubectl get nodes -n argocd &&
+sudo kubectl get svc -n argocd &&
+
 # OR https://bcrypt-generator.com/
 # sudo kubectl -n argocd patch secret argocd-secret
 #   -p '{"stringData":  {
@@ -63,17 +74,20 @@ sleep 30 #??? for pods gets up
 
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443  --address=0.0.0.0 > /dev/null 2>&1 &
 
-sudo kubectl create namespace dev &&
-
 sudo kubectl apply -f /home/vagrant/vagrant/confs/project.yaml -n argocd
 sudo kubectl apply -f /home/vagrant/vagrant/confs/app.yaml -n argocd
-
 
 # # check nodes on argocd namespace
 # # kubectl get pods -n argocd
 
+
+# Получить пароль в декодированном виде:
+echo "==========get password=========="
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d ; echo
+echo "================================"
+
 # argocd login localhost:8080
 # argocd app create playground --repo https://github.com/bumblebee-di/inception-of-things-p3.git --path manifests --dest-server https://kubernetes.default.svc --dest-namespace dev
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d ; echo
+# sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d ; echo
 
 # sudo kubectl port-forward app-deployment-85dd49fff8-v6kgj -n dev 8888:8888 --address-0.0.0.0 > /dev/null 2>&1
--- p3/run.sh
+++ p3/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Set the color variable
 green='\033[0;32m'
+red='\033[0;31m'
 # Clear the color after that
 clear='\033[0m'
 
 BOXURL="https://app.vagrantup.com/debian/boxes/bullseye64/versions/11.20221219.1/providers/virtualbox.box"
-GOINFRE="/opt/goinfre/bgoat"
+GOINFRE="/opt/goinfre/$USER"
 BOXPATH=$GOINFRE"/debian_bullseye64"
 
 
@@ -14,14 +15,18 @@ if [ -n "$1" -a "$1" == "f" ]; then {
         printf "${green}$BOXPATH exists${clear}\n"
     }
     else {
-        printf "${green}Downloading from $BOXURL to $BOXPATH${clear}\n"
-        curl -o $BOXPATH.downloading $BOXURL &&
-        mv $BOXPATH.downloading $BOXPATH
+        printf "${red}$BOXPATH does not found. Download it from $BOXURL${clear}\n"
+        exit -1
+        # printf "${green}Downloading from $BOXURL to $BOXPATH${clear}\n"
+        # curl -o $BOXPATH.downloading $BOXURL &&
+        # mv $BOXPATH.downloading $BOXPATH
     }
     fi
     if ! test -d $GOINFRE/.vagrant.d; then {
+        printf "${green}$GOINFRE/.vagrant.d not found${clear}\n"
         mkdir $GOINFRE/.vagrant.d
         if test -d $HOME/.vagrant.d; then {
+            printf "${green}$HOME/.vagrant.d exists${clear}\n"
             mv $HOME/.vagrant.d/* $GOINFRE/.vagrant.d/
             rm -f $HOME/.vagrant.d
             ln -s $GOINFRE/.vagrant.d $HOME/.vagrant.d
@@ -35,10 +40,10 @@ if [ -n "$1" -a "$1" == "f" ]; then {
         fi
     }
     fi
-    export VAGRANT_HOME="$HOME/goinfre/vagrant"
+    export VAGRANT_HOME="$HOME/goinfre/.vagrant.d"
+    printf "${green}Adding Vagrant box debian_bullseye63${clear}\n"
     vagrant box add debian_bullseye64 ~/goinfre/debian_bullseye64   
-    
-    
+
     vagrant up
 
     CAT1=$(cat ~/.ssh/known_hosts)
@@ -51,6 +56,9 @@ if [ -n "$1" -a "$1" == "f" ]; then {
         }
         fi
     done
+    printf "${green}Copying yamls to ~/confs${clear}\n"
+    scp -r -P 2222 -o StrictHostKeyChecking=no ./confs vagrant@localhost:. 
+
     printf "${green}Выполняем развертывание${clear}\n"
     ssh -o StrictHostKeyChecking=no -q vagrant@localhost -p 2222 "bash -s" < ./p3.sh
     # ssh -o StrictHostKeyChecking=no -q vagrant@localhost -p 2222 "bash -s" < ./scripts/cluster.sh
