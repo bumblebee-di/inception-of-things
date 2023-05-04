4af7f156c1d2f0ffa84d52f013522d60c1ea9e94
Crabb Marsha <cmarsha@ge-l6.kzn.21-school.ru>
2023-05-04 23:53:59 +0300

    some beauty

--- bonus/scripts/gitlab.sh
+++ bonus/scripts/gitlab.sh
@@ -8,4 +8,4 @@ sudo helm upgrade --install -n gitlab gitlab gitlab/gitlab -f values-minimal.yam
 
 sudo git clone https://github.com/bumblebee-di/inception-of-things-p3.git
 
-cd inception-of-things-p3 && sudo git remote set-url origin http://192.168.56.110.nip.io:8080/iot.git
+cd inception-of-things-p3 && sudo git remote set-url origin http://192.168.56.110.nip.io:8080/root/iot.git
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -16,12 +16,12 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
-printf "==========\nInstalling docker\n==========\n"
+printf "==========\n\033[32mInstalling docker\033[0m\n==========\n"
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh &&
 
 sudo usermod -aG docker $USER
 
-printf "==========\nInstalling kubectl\n==========\n"
+printf "==========\n\033[32mInstalling kubectl\033[0m\n==========\n"
 # Download latest stable version of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
 # Download sha256 hash sum for just installed kubectl and check it
@@ -31,7 +31,7 @@ echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check &&
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
 
 # Install k3d
-printf "==========\nInstalling k3d\n==========\n"
+printf "==========\n\033[32mInstalling k3d\033[0m\n==========\n"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash &&
 
 # Add autocompletion
@@ -39,7 +39,7 @@ echo "source <(k3d completion bash)" >> ~/.bashrc
 source ~/.bashrc
 
 # Install Helm
-printf "==========\nInstalling Helm\n==========\n"
+printf "==========\n\033[32mInstalling Helm\033[0m\n==========\n"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 &&
 sudo chmod 700 get_helm.sh &&
 sudo ./get_helm.sh &&
@@ -47,42 +47,53 @@ sudo ./get_helm.sh &&
 # Install cluster
 # https://www.sokube.io/en/blog/gitops-on-a-laptop-with-k3d-and-argocd-en
 
-printf "==========\nCreating cluster\n==========\n"
+printf "==========\n\033[32mCreating cluster\033[0m\n==========\n"
 # sudo k3d cluster create mycluster -p "8080:30080@agent:0" --agents 2
 sudo k3d cluster create iot -p "8888:30080@agent:0" --agents 1 &&
-printf "==========\nCreating namespaces\n==========\n"
+printf "==========\n\033[32mCreating namespaces\033[0m\n==========\n"
 sudo kubectl create namespace argocd &&
 sudo kubectl create namespace dev &&
 sudo kubectl create namespace gitlab &&
 
 # Deploy the GitLab Helm chart
 # Install GitLab
-printf "==========\nInstalling GitLab\n==========\n"
-helm repo add gitlab https://charts.gitlab.io/ &&
+printf "==========\n\033[32mSetting gitlab repo\033[0m\n==========\n"
+while 
+  sudo helm repo add gitlab https://charts.gitlab.io/
+  [ $? -gt 0 ]
+do :; done
+sudo helm repo list
 sudo helm repo update
-sudo helm upgrade --install gitlab gitlab/gitlab \
+# while [ $? -gt 0 ]; do
+while [[ ! "$(sudo helm repo update)" =~ "...Successfully got an update from the \"gitlab\" chart repository" ]]; do
+  echo 'Trying to update helm repo...'
+done
+printf "==========\n\033[32mInstalling GitLab\033[0m\n==========\n"
+sleep 20
+sudo helm install gitlab gitlab/gitlab -n gitlab \
+  --set global.ingress.configureCertmanager="false" \
+  --set certmanager-issuer.email=sibogatovrm@gmail.com \
   --timeout 600s \
-  --set global.hosts.domain=example.com \
-  --set global.hosts.externalIP=10.10.10.10 \
-  --set certmanager-issuer.email=me@example.com \
-  --set postgresql.image.tag=13.6.0
+  --set global.hosts.domain=192.168.56.11.nip.io \
+  --set global.hosts.externalIP=192.168.56.110 \
+  --set global.hosts.https=false \
+  --set gitlab-runner.install="false" &&
 
 # Install ArgoCD into argocd NAMESPACE
 # https://yashguptaa.medium.com/application-deploy-to-kubernetes-with-argo-cd-and-k3d-8e29cf4f83ee
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml &&
-printf "==========\nWaiting for pods\n"
+printf "==========\n\033[32mWaiting for pods\033[0m\n==========\n"
 sleep 40 && #??? for pods gets up
-printf "==========\n"
 # OR
 # sudo kubectl wait --for=condition=Ready pods --all -n argocd &&
 
 # check nodes on argocd namespace
-printf "==========\nGet pods\n==========\n"
+printf "==========\n\033[32mGet pods\033[0m\n==========\n"
 sudo docker ps &&
 sudo kubectl get pods -n argocd &&
 sudo kubectl get nodes -n argocd &&
-sudo kubectl get svc -n argocd &&
+sudo kubectl get svc -n argocd #&&
 
 # OR https://bcrypt-generator.com/
 # sudo kubectl -n argocd patch secret argocd-secret
@@ -91,9 +102,13 @@ sudo kubectl get svc -n argocd &&
 #     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
 #   }}'
 
+sudo git clone https://github.com/bumblebee-di/inception-of-things-p3.git &&
+cd inception-of-things-p3 && sudo git remote set-url origin http://192.168.56.110.nip.io:8080/root/iot.git &&
+
+
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443  --address=0.0.0.0 > /dev/null 2>&1 &
 
-sudo kubectl apply -f /home/vagrant/confs/project.yaml -n argocd
+# sudo kubectl apply -f /home/vagrant/confs/project.yaml -n argocd
 sudo kubectl apply -f /home/vagrant/confs/app.yaml -n argocd
 
 # # check nodes on argocd namespace
