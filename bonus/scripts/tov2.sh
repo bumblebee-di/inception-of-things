39b290dff61ce615c69db51fa8ec787dbaf07bf1
Bumblebee Goat <bgoat@ge-l4.kzn.21-school.ru>
2023-05-12 22:44:15 +0300

    prettier

--- bonus/scripts/tov2.sh
+++ bonus/scripts/tov2.sh
@@ -1,5 +1,5 @@
 cd /home/vagrant/inception-of-things-p3
-sed -i 's/wil42\/playground\:v2/wil42\/playground\:v1/g' ./manifests/deployment.yaml 
+sed -i 's/wil42\/playground\:v1/wil42\/playground\:v2/g' ./manifests/deployment.yaml 
 git add manifests/deployment.yaml
 git commit -m "v1"
-git push http://root:${GIT_PASS}@gitlab.192.168.56.110.nip.io:8080/root/inception-of-things-p3.git 
+export GIT_PASS=$(sudo kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 -d ) ; git push http://root:${GIT_PASS}@gitlab.192.168.56.110.nip.io:8080/root/inception-of-things-p3.git 
